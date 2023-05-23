41a022ba702f8e32418b65b8007150cb467e7345
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-05-23 12:21:57 +1200

    Created POST routes in express

--- src/app.js
+++ src/app.js
@@ -7,4 +7,33 @@ const generateId = require("./generateId");
 
 app.use(express.json());
 
+app.post("/wishlists", (request, response) => {
+    const { body } = request;
+    const wishlists = {
+        id: generateId(),
+        productId: body.productId,
+    };
+    return response.status(200).send(wishlists);
+});
+
+app.post("/orders", (request, response) => {
+    const { body } = request;
+    const order = {
+        id: generateId(),
+        ...body,
+    };
+    return response.status(200).send(order);
+});
+
+
+app.post("/policies", (request, response) => {
+    const { body } = request;
+    const policies = {
+        id: generateId(),
+        ...body,
+    };
+    return response.status(200).send(policies);
+});
 module.exports = app;
+
+
